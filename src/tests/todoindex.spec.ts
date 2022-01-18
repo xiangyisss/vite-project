e9ba0f2de5232cfe64c429b913169c9a712cdebd
xiangyisss <xiangyisunn@gmail.com>
2022-01-18 01:03:24 +0100

    pinia test setup down

--- src/tests/todoindex.spec.ts
+++ src/tests/todoindex.spec.ts
@@ -1,8 +1,17 @@
-// import { shallowMount } from '@vue/test-utils';
-// import TodoIndex from '../pages/Todo/TodoIndex.vue'
+import { shallowMount } from '@vue/test-utils';
+import TodoIndex from '../pages/Todo/TodoIndex.vue'
+// import { createPinia, setActivePinia } from 'pinia' 
+import { createTestingPinia } from '@pinia/testing'
+import {todoStore} from '../stores/Todo/todo'
+
 
 describe('TodoIndex Test Case', () => {
-    it('just resolves with true', () => {
-        expect(true).toEqual(true)
+    it('find first h1', () => {
+        const wrapper = shallowMount(TodoIndex, {
+            global: {
+                plugins: [createTestingPinia()]
+            }
+        })
+        expect(wrapper.find('h1').text()).toEqual('Write it down your todays task')
     })
 })
